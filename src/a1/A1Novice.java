57340598030ec401a51dbd098b4280979ffd91ec
mada <mada@mada-pc>
2019-08-25 15:22:15 -0400

    Added novice1 code

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -9,6 +9,32 @@ public class A1Novice {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		// integer indicating total number of customers
+		int count = scan.nextInt();
+		
+		//creates an array to store customer names
+		String[] customer = new String[count];
+		
+		//reads through each customers order
+		for (int i=0; i<customer.length; i++) {
+			String fname = scan.next();
+			String lname = scan.next();
+			int items = scan.nextInt();
+			double total = 0;
+			//sets amounts, names, and prices to the items bought
+			for (int j=0; j<items; j++) {
+				int amount = scan.nextInt();
+				String name = scan.next();
+				double price = scan.nextDouble();
+				// calculates total price based on items bought
+				total = amount*price + total;
+				// print results
+				System.out.println(fname + lname + ":" + total);
+			}
+			
+			
+			}
+		}
 		
 	}
-}
+
