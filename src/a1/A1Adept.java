57d1b8f993d4dace325d6b897af20f7821f520ba
mada <mada@host.docker.internal>
2019-08-26 21:59:39 -0400

    only have output: average

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -7,8 +7,63 @@ public class A1Adept {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
+		// create a class representing items available and their prices
+		class ItemStuff {
+			String foodies = "";
+			double price = 0;
+		}
+		// create a class representing customers and amount of money they spend
+		class Customers {
+			String fname = "";
+			String lname = "";
+			double totalPrice = 0;
+		}
+		// set initial value for "biggest" and "average" to be used later
+		double biggest = 0;
+		double average = 0;
+		// create count of how many items are in the store
+		int itemcount = scan.nextInt();
+		// create an array to store each item along with their properties (name and price)
+		ItemStuff storeItems[] = new ItemStuff[itemcount];
+		for (int i=0; i<itemcount; i++) {
+			storeItems[i].foodies = "rawr";
+			storeItems[i].price = 0;
+			storeItems[i].foodies = scan.next();
+			storeItems[i].price = scan.nextDouble();
+		}
+		// create a count for the number of customers
+		int peoples = scan.nextInt();
+		// create an array to store each customer and their orders
+		Customers custItems[] = new Customers[peoples];
+		// a for loop that goes through the customers and their orders one at a time
+		for (int j=0; j<peoples; j++) {
+			custItems[j].fname = scan.next();
+			custItems[j].lname = scan.next();
+			// create a count for amount of items bought by customer
+			int ibuy = scan.nextInt();
+			// new variable in for loop to loop through each item bought
+			for (int x=0; x<ibuy; x++) {
+				// indicate how much of each item was bought
+				int amount = scan.nextInt();
+				// sets a name for each item bought to relate to their price
+				String itemname = scan.next();
+				// new variable "y" to relate the price and names
+				for (int y=0; itemname == storeItems[y].foodies; y++)
+					custItems[j].totalPrice = amount*storeItems[y].price;
+				} 
+			// finds the total cost of all the customers combined
+			average = custItems[j].totalPrice + average;
+			// finds the highest amount spent
+			if (custItems[j].totalPrice > biggest) {
+				biggest = custItems[j].totalPrice;
+			// finds the lowest amount spent
+			} else if (custItems[j].totalPrice < biggest) {
+				double smallest = custItems[j].totalPrice;
+			}
 		
+		}
+
+System.out.println("Average: " + average/peoples);
+
 	}
-}
+};
