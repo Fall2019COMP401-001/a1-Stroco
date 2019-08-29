785601cdef2564ae52cf2a6706159572ff1bbced
mada <mada@mada-pc.wireless-1x.unc.edu>
2019-08-28 23:24:56 -0400

    Finally finished

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -9,6 +9,41 @@ public class A1Jedi {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
-		
+		int storeItems = scan.nextInt();
+		int tempInt = 0;
+		int tempInt2 = 0;
+		String stringarr[] = new String[storeItems];
+		int intarr[] = new int[storeItems];
+		for (int i = 0; i < storeItems; i++) {
+			String foodname = scan.next();
+			double price = scan.nextDouble();
+			stringarr[i] = foodname;
+		}
+		int peoples = scan.nextInt();
+		int peoplesarr[] = new int[storeItems];
+		for (int i = 0; i < peoples; i++) {
+			String fname = scan.next();
+			String lname = scan.next();
+			int amount = scan.nextInt();
+			for (int j = 0; j < amount; j++) {
+				int quant = scan.nextInt();
+				String name = scan.next();
+				for (int x = 0; x < storeItems; x++) {
+					if (name.contentEquals(stringarr[x])) {
+						peoplesarr[x]++;
+						intarr[x] += quant; 
+					}
+				}
+			}
+		}
+		for (int x = 0; x < storeItems; x++) {
+				if (peoplesarr[x] == 0) {
+					System.out.println("No customers bought " + stringarr[x]);
+				} else {
+					System.out.println(peoplesarr[x] + " customers bought " + intarr[x] + " " + stringarr[x]);
+				}
+			}
+		}
 	}
-}
+
+
